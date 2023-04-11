c602e3a142d498cb66f6f19d59851ff9eae315e5
thanhtu02 <hanhee1406@gmail.com>
2023-04-11 20:41:03 +0700

    First commited
    first commited

---
+++ app/src/main/java/com/example/lab2_20522104/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.lab2_20522104;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import android.widget.TextView;
+import android.widget.Button;
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView, textView1, list;
+    EditText inputText, inputText1;
+    Button submit;
+
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView); //ten
+        inputText = findViewById(R.id.inputText);
+
+        textView1 = findViewById(R.id.textView1); //gross salary
+        inputText1 = findViewById(R.id.inputText1);
+
+        list = findViewById(R.id.list); //danh sach ten - net salary sau khi onclick vao btn calculate
+        submit = findViewById(R.id.submit);
+
+        SharedPreferences sharePreferences = getSharedPreferences("dataSalary", MODE_PRIVATE);
+        inputText.setText(sharePreferences.getString("name", ""));
+        inputText1.setText(sharePreferences.getString("netsalary", ""));
+
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void onUpdate(View view) {
+
+        String name = inputText.getText().toString().trim();
+
+        double grossSalary = Double.parseDouble(inputText1.getText().toString());
+        double dependentCost = 11000000;
+
+        double temp = grossSalary * (1 - 0.105);
+        String netSalary1 = String.valueOf(temp);
+
+        double temp2 = dependentCost + (temp - dependentCost) * (1 - 0.05);
+        String netSalary2 = String.valueOf(temp2);
+
+        SharedPreferences sharePreferences = null;
+        if (temp <= dependentCost) {
+            list.setText(inputText.getText() + " - Net Salary: " + netSalary1);
+            assert false;
+            SharedPreferences.Editor editor = sharePreferences.edit();
+            editor.putString("name", name);
+            editor.putString("netsalary", netSalary1);
+            editor.apply();
+        } else {
+            list.setText(inputText.getText() + " - Net Salary: " + netSalary2);
+            assert false;
+            SharedPreferences.Editor editor = sharePreferences.edit();
+            editor.putString("name", name);
+            editor.putString("netsalary", netSalary2);
+            editor.apply();
+        }
+    }
+}
